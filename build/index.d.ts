6a209a423f80922a8248f775ae76a550dd4f6a55
James-Alex Matthews <vamtiger@vamtiger.com>
2018-12-01 14:46:13 +0200

    vamtiger-node-typescript-commit

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="node" />
 import { PathLike } from 'fs';
-export { default as DirectoryContent, ClassifiedDirectoryContent } from './directory-content';
+export { ClassifiedDirectoryContent } from './directory-content';
 declare const _default: (params: IParams) => Promise<import("../../../../../../../Users/vamtiger/Documents/Programming/VamtigerProject/vamtiger-get-directory-content-recursive/source/directory-content").Result>;
 export default _default;
 export interface IParams {
